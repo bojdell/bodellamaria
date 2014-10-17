9ff1ed26649688b18203fb193890c4d20a429b28
Bodecker DellaMaria <bojdell@gmail.com>
2014-10-17 00:17:45 -0500

    added angular QOD support

--- public/js/index_controllers.js
+++ public/js/index_controllers.js
@@ -1,3 +1,5 @@
 function QODController($scope) {
-    $scope.QOD = "hello";
+    $scope.quote = "Far better is it to dare mighty things, to win glorious triumphs, even though checkered by failure... than to rank with those poor spirits who neither enjoy nor suffer much, because they live in a gray twilight that knows not victory nor defeat.";
+    $scope.person = "Theodore Roosevelt"
+    $scope.source = "http://www.brainyquote.com/quotes/quotes/t/theodorero103499.html"
 }
