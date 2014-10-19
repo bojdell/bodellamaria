c18f0a20c58dbdee936ee1403bb0f34bf4ee4eba
Bodecker DellaMaria <bojdell@gmail.com>
2014-10-18 20:24:29 -0500

    added quotes page

--- public/js/quotes_controllers.js
+++ public/js/quotes_controllers.js
@@ -1,3 +1,11 @@
 function QuotesController($scope) {
-    $scope.quotes = {};
+    $scope.quotes = $http.get("http://bodecker.me/api/quotes");
+    // $scope.quotes = [{"_id":"543da325e7b6b9f9751ad2ae","text":"Far better is it to dare mighty things, to win glorious triumphs, even though checkered by failure... than to rank with those poor spirits who neither enjoy nor suffer much, because they live in a gray twilight that knows not victory nor defeat.","person":"Theodore Roosevelt","source":"http://www.brainyquote.com/quotes/quotes/t/theodorero103499.html"},{"_id":"543da412e7b6b9f9751ad2af","text":"This is your life and it's ending one minute at a time.","person":"Tyler Durden","source":"https://chrome.google.com/webstore/detail/news-feed-eradicator-for/fjcldmjmjhkklehbacihaiopjklihlgg?hl=en"}];
 }
+
+// var $grid = $('.grid');
+// $grid.cloudGrid({
+//     gridSize: 60,
+//     gridGutter: 13,
+//     children: $grid.children('.gridItem')
+// });
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var MongoClient = require('mongodb').MongoClient
 	, assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/quotesdb';
+var url = 'mongodb://bodecker.me:27017/quotesdb';
 var connection = null;
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
@@ -20,9 +20,13 @@ app.get("/", function (req, res) {
 	res.redirect("/index.html");
 });
 
+app.get("/quotes", function (req, res) {
+	res.redirect("/quotes.html");
+});
+
 app.get("/api/quotes", function (req, res) {
-    var collection = connection.get('quotes');
-	res.send(collection.find({}))
+    var quotes = connection.get('quotes');
+	res.send(quotes.find({}))
 });
 
 app.listen(80);
