f1890e233c3e5f5595342e11bfe08b0584449468
Bodecker DellaMaria <bojdell@gmail.com>
2014-11-02 18:52:28 -0600

    adding server

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var MongoClient = require('mongodb').MongoClient
 	, assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://bodecker.me:27017/quotesdb';
+// var url = 'mongodb://bodecker.me:27017/quotesdb';
 var connection = null;
 // Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-	assert.equal(null, err);
-	console.log("Connected correctly to server");
-	connection = db;
-});
+// MongoClient.connect(url, function(err, db) {
+// 	assert.equal(null, err);
+// 	console.log("Connected correctly to server");
+// 	connection = db;
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -20,17 +20,29 @@ app.get("/", function (req, res) {
 	res.redirect("/index.html");
 });
 
+app.get("/about", function (req, res) {
+	res.redirect("/about.html");
+});
+
+app.get("/projects", function (req, res) {
+	res.redirect("/projects.html");
+});
+
+app.get("/music", function (req, res) {
+	res.redirect("/music.html");
+});
+
 app.get("/quotes", function (req, res) {
 	res.redirect("/quotes.html");
 });
 
-app.get("/api/quotes", function (req, res) {
-    var collection = connection.collection('quotes');
-    collection.find({}).toArray(function(err, docs) {
-        assert.equal(err, null);
-	res.send(docs);
-    });
-});
+// app.get("/api/quotes", function (req, res) {
+//     var collection = connection.collection('quotes');
+//     collection.find({}).toArray(function(err, docs) {
+//         assert.equal(err, null);
+// 	res.send(docs);
+//     });
+// });
 
 app.listen(80);
 console.log("Listening on port 80");
