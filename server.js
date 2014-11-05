0e3219289debbf2c5aff2f5da0185096c04ab233
bodecker.cloudapp.net <bojdell@gmail.com>
2014-11-05 05:01:20 +0000

    trying to merge lol

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var MongoClient = require('mongodb').MongoClient
 	, assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://bodecker.me:27017/quotesdb';
+var url = 'mongodb://localhost:27017/quotesdb';
 var connection = null;
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
@@ -14,10 +14,31 @@ MongoClient.connect(url, function(err, db) {
 	connection = db;
 });
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 app.use(express.static(__dirname + '/public'));
+app.use(allowCrossDomain);
 
 app.get("/", function (req, res) {
 	res.redirect("/index.html");
+});
+
+app.get("/about", function (req, res) {
+        res.redirect("/about.html");
+});
+
+app.get("/projects", function (req, res) {
+        res.redirect("/projects.html");
+});
+
+app.get("/music", function (req, res) {
+        res.redirect("/music.html");
 });
 
 app.get("/quotes", function (req, res) {
@@ -28,8 +49,9 @@ app.get("/api/quotes", function (req, res) {
     var collection = connection.collection('quotes');
     collection.find({}).toArray(function(err, docs) {
         assert.equal(err, null);
-	res.send(docs);
+        res.send(docs);
     });
+//    res.send(quotes.find({}));
 });
 
 app.listen(80);
