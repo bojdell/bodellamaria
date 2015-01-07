20f539f9c69513e46793064b2a252b0f0e26db3b
Bodecker DellaMaria <bojdell@gmail.com>
2015-01-06 22:58:19 -0600

    refactored code

--- archive/server.js
+++ archive/server.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
 
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+// var MongoClient = require('mongodb').MongoClient
+//     , assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/quotesdb';
+// var url = 'mongodb://localhost:27017/quotesdb';
 var connection = null;
 // Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-    connection = db;
-});
+// MongoClient.connect(url, function(err, db) {
+//     assert.equal(null, err);
+//     console.log("Connected correctly to server");
+//     connection = db;
+// });
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost');
