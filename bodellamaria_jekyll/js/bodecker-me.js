70628eba5905b36ee8346ac15f7319f8f035dc55
Bodecker DellaMaria <bojdell@gmail.com>
2015-02-14 15:26:30 -0600

    added custom js

---
+++ bodellamaria_jekyll/js/bodecker-me.js
@@ -0,0 +1,29 @@
+$(".caption-icon").hover(
+	function() {
+    	$(".caption-msg").show();
+	},
+	function() {
+		$(".caption-msg").hide();
+	}
+);
+
+$(".caption-icon").click(
+	function() {
+    	$(".caption-msg").show();
+	}
+);
+
+// $(".photo-caption").hover(
+// 	function() {
+//     	$(".caption-msg").show();
+// 	},
+// 	function() {
+// 		$(".caption-msg").hide();
+// 	}
+// );
+
+// $(".photo-caption").click(
+// 	function() {
+//     	$(".caption-msg").show();
+// 	}
+// );
