ba06d66c9a4b5d238614fd2d04cb13ff2d4c70a2
Bodecker DellaMaria <bojdell@gmail.com>
2015-02-15 17:09:15 -0600

    fixed some button js

--- bodellamaria_jekyll/js/bodecker-me.js
+++ bodellamaria_jekyll/js/bodecker-me.js
@@ -1,29 +1,55 @@
-$(".caption-icon").hover(
-	function() {
-    	$(".caption-msg").show();
-	},
-	function() {
-		$(".caption-msg").hide();
+var override_caption = false;
+
+// default CSS from bodecker-me.css (initialized via readCSS())
+var msg_css_hidden = [];
+var icon_css_hidden = [];
+
+// CSS to set on click
+var msg_css_visible = [
+	["visibility", "visible"]
+];
+var icon_css_visible = [
+	["border-top-right-radius", "0em"]
+];
+
+$(document).ready(function(){
+	readCSS();
+	$(".caption-icon").hover(showCaption, hideCaption);
+	$(".caption-icon").click(overrideCaption);
+	$(".caption-msg").click(overrideCaption);
+});
+
+// read default CSS values
+function readCSS() {
+	var i;
+	for(i = 0; i < msg_css_visible.length; i++) {
+		var property = msg_css_visible[i][0];
+		msg_css_hidden.push( [property, $(".caption-msg").css(property)] );
 	}
-);
-
-$(".caption-icon").click(
-	function() {
-    	$(".caption-msg").show();
+	for(i = 0; i < icon_css_visible.length; i++) {
+		var property = icon_css_visible[i][0];
+		icon_css_hidden.push( [property, $(".caption-msg").css(property)] );
 	}
-);
+}
 
-// $(".photo-caption").hover(
-// 	function() {
-//     	$(".caption-msg").show();
-// 	},
-// 	function() {
-// 		$(".caption-msg").hide();
-// 	}
-// );
+function showCaption() {
+	if(!override_caption) {
+		$(".caption-msg").css("visibility","visible");
+		$(".caption-icon").css("border-top-right-radius","0em");
+		$(".caption-icon").css("border-top-left-radius","0em");
+		$(".caption-icon").css("opacity",".8");
+	}
+}
 
-// $(".photo-caption").click(
-// 	function() {
-//     	$(".caption-msg").show();
-// 	}
-// );
+function hideCaption() {
+	if(!override_caption) {
+		$(".caption-msg").css("visibility","hidden");
+		$(".caption-icon").css("border-top-right-radius","0.5em");
+		$(".caption-icon").css("border-top-left-radius","0.5em");
+	}
+}
+
+function overrideCaption() {
+	override_caption = !override_caption;
+	hideCaption();		// does nothing if override_caption is set
+}
